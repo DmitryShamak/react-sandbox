46d9aa5260ebf6a8eb377f7618d4de9e73b4e7b5
DmitryShamak <hellkitedmitry@mail.ru>
2016-08-12 12:35:23 +0300

    updates for sign in form

--- app/js/landing.js
+++ app/js/landing.js
@@ -1,90 +1,7 @@
-function Landing() {
-    var container = document.createElement("div");
-        container.id = "landing-canvas";
-
-    var self = this,
-        isMounted;
-
-    var form = {};
-
-    self.signIn = function() {
-        if(!form.email || !form.email.valid || !form.password || !form.password.valid) {
-            return;
-        }
-
-        form = {};
-        $state && $state.go("map");
-    };
-
-    function onChange(key, event) {
-        var target = event.target;
-        var value = target.value.trim();
-        var valid = value != "";
-
-        $(target).removeClass("invalid");
-
-        form[key] = {
-            valid: valid,
-            value: value,
-            target: target
-        };
-
-        if(!valid) {
-            $(target).addClass("invalid");
-        }
-
-        target.value = value;
-        return value;
-    }
-
-    var Component = React.createClass({
-        render: function() {
-            return (
-                <div className="account no-selection text-center margin-top-sm">
-                    <h2>Landing</h2>
-
-                    <div className="fixed-xs-width">
-                        <div className="form-group">
-                            <input className="form-control" type="text" onChange={onChange.bind(this, "email")} placeholder="email" />
-                        </div>
-                        <div className="form-group">
-                            <input className="form-control" type="password" onChange={onChange.bind(this, "password")} placeholder="password" />
-                        </div>
-                        <div className="btn form-control" onClick={self.signIn}>Sign In</div>
-                    </div>
-                </div>
-            );
-        }
-    });
-
-    var init = function() {
-        ReactDOM.render(
-            <Component />,
-            container
-        );
-
-        isMounted = true;
-        document.body.appendChild(container);
-    };
-
-    self.remove = function() {
-        if(!isMounted) {
-            return;
-        }
-
-        isMounted = false;
-        ReactDOM.unmountComponentAtNode(container);
-        document.body.removeChild(container);
-    };
-
-    self.render = function() {
-        init();
-        console.log("Landing rendered");
-    }
-}
-
 module.exports = React.createClass({
     getInitialState: function() {
+      $(document).bind("submit", "form", function(e) {e.preventDefault();} );
+
       return {
         form: {},
         onchange: function(key, event) {
@@ -108,7 +25,9 @@ module.exports = React.createClass({
             target.value = value;
             return value;
         },
-        signin: function() {
+        signin: function(e) {
+            e.preventDefault();
+
             var form = this.state.form;
             if(!form.email || !form.email.valid || !form.password || !form.password.valid) {
                 return;
@@ -130,15 +49,15 @@ module.exports = React.createClass({
             <div className="account no-selection text-center module-account">
                 <h2>Landing</h2>
 
-                <div className="fixed-xs-width">
+                <form className="fixed-xs-width" onSubmit={this.state.signin.bind(this)}>
                     <div className="form-group">
                         <input className="form-control" type="text" onChange={this.state.onchange.bind(this, "email")} placeholder="email" />
                     </div>
                     <div className="form-group">
                         <input className="form-control" type="password" onChange={this.state.onchange.bind(this, "password")} placeholder="password" />
                     </div>
-                    <div className="btn form-control" onClick={this.state.signin.bind(this)}>Sign In</div>
-                </div>
+                    <button className="btn form-control" onClick={this.state.signin.bind(this)}>Sign In</button>
+                </form>
             </div>
         );
     }
